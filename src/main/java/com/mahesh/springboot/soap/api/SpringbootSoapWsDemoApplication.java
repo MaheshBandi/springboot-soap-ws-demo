1a241ada38c15dd186e1dfa05e66053cdac648ea
Mahesh Bandi <mbandi1985@gmail.com>
2022-12-12 01:15:45 +0530

    initial commit

---
+++ src/main/java/com/mahesh/springboot/soap/api/SpringbootSoapWsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mahesh.springboot.soap.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSoapWsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSoapWsDemoApplication.class, args);
+	}
+
+}
